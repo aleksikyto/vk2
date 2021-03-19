5c074f5eba605cdda60a925f0f417a10ac7645f9
aleksikyto <aleksi.kyto@hotmail.com>
2021-03-19 10:37:15 +0200

    'router'

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+const port = 3000;
+const catRouter = require('./r/catRouter')
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/cat', catRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http:\\localhost:${port}`);
+});
---
+++ r/catRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('get cat');
+  res.send('Hello Cat!');
+});
+
+router.post('/', (req, res) => {
+  console.log('post cat');
+  res.send('post cat');
+});
+
+router.put('/', (req, res) => {
+  console.log('put cat');
+  res.send('put cat');
+});
+
+router.delete('/', (req, res) => {
+  console.log('delete cat');
+  res.send('delete cat');
+});
+
+module.exports = router;
+
